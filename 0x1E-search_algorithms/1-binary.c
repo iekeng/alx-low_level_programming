05dedb71cd93af2c74d1e1a92efc82295d525087
iekeng <ekeng7.eb@gmail.com>
2023-05-12 20:37:59 +0100

    binary search algorithm..

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,10 +22,10 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
-			printf("%d\n", array[i]);
+			printf("%d", array[i]);
 			if (i < right)
 				printf(", ");
 
