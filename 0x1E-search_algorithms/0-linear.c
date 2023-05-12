6504dea9e909aaa50479105e5d4a8a7aa2064ae6
iekeng <ekeng7.eb@gmail.com>
2023-05-12 14:38:05 +0100

    lnear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "search_algos.h"
 
-/*
-* linear_search - searches an array, returns the index
+/**
+*linear_search - searches an array, returns the index
 * when an element identical to @value is found.
+*
 * @array: pointer to the address of the first array element.
 * @size: size of the array.
 * @value: value to search for.
-*/
+*
+* Return: the index of the matching element in the array
+*		or -1 if the execution is unsuccessful.
+**/
 
 int linear_search(int *array, size_t size, int value)
 {
@@ -17,8 +21,9 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 
 	for (i = 0; i < size; i++)
+	{
 		if (*array++ == value)
 			return (i);
-	else
-		return (-1);
+	}
+	return (-1);
 }
