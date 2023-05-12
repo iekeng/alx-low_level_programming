4af8fea2a2cb20cd9ad1cf935e932925f53ad45f
iekeng <ekeng7.eb@gmail.com>
2023-05-12 15:10:41 +0100

    linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -22,6 +22,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%d]\n", i, *array++);
 		if (*array++ == value)
 			return (i);
 	}
